30f7c751958e9007eeb4f5f79227ba3a04da9cc1
gonzalo <gtercerogarcia@gmail.com>
2018-01-15 18:57:28 +0100

    avanzamos con animaciones

---
+++ Animation/Assets/Scripts/Animation/AnimController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+
+	private Animator anim;	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float verticalMovement = Input.GetAxisRaw ("Vertical");
+		if (verticalMovement != 0.0f) {
+			anim.SetBool ("esta_andando", true);
+			anim.SetFloat ("speed", verticalMovement);
+
+		} else {
+			anim.SetBool ("esta_andando", false);
+		}
+
+		if (Input.GetKey ("space")) {
+			anim.SetBool ("esta_saltando", true);
+		} else {
+			anim.SetBool ("esta_saltando", false);
+		}
+		
+	}
+}
---
+++ Animation/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		float verticalMovement = Input.GetAxis ("Vertical");
+		if (verticalMovement != 0.0f) {
+			transform.Translate (transform.forward * verticalMovement * Time.deltaTime);
+		}
+	}
+}
