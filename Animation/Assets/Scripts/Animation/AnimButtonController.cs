98dbf7683ef670b099389b6b280f364bdbb3c126
gonzalo <gtercerogarcia@gmail.com>
2018-01-22 18:59:13 +0100

    faltan modificar botones

--- Animation/Assets/Scripts/Animation/AnimButtonController.cs
+++ Animation/Assets/Scripts/Animation/AnimButtonController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimController : MonoBehaviour {
+public class AnimButtonController : MonoBehaviour {
 
 	private Animator anim;
 	private Rigidbody rb;
@@ -11,10 +11,10 @@ public class AnimController : MonoBehaviour {
 		anim = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		float verticalMovement = Input.GetAxisRaw ("Vertical");
+		/*
 		if (verticalMovement != 0.0f) {
 			anim.SetBool ("esta_andando", true);
 			anim.SetFloat ("speed", verticalMovement);
@@ -22,23 +22,44 @@ public class AnimController : MonoBehaviour {
 		} else {
 			anim.SetBool ("esta_andando", false);
 		}
-
+*/
 		if (Input.GetKeyDown ("space")) {
 			rb.AddForce (new Vector3(0.0f, 10.0f, 0.0f), ForceMode.Impulse);
 			anim.SetBool ("esta_saltando", true);
 		}
-		
+
 	}
 
 
+	public void moveForward(){
+		anim.SetBool ("esta_andando", true);
+		anim.SetFloat ("speed", 1.0f);
+	}
+
+	public void moveBack(){
+		anim.SetBool ("esta_andando", true);
+		anim.SetFloat ("speed", -1.0f);
+	}
+
+	public void moveLeft(){
+		anim.SetBool ("esta_andando", true);
+		anim.SetFloat ("speed", 1.0f);
+	}
+
+	public void moveRight(){
+		anim.SetBool ("esta_andando", true);
+		anim.SetFloat ("speed", 1.0f);
+	}
+
+	public void stop(){
+		anim.SetBool ("esta_andando", false);
+	}
 	public void Jump(){
 		rb.AddForce (new Vector3(0.0f, 10.0f, 0.0f), ForceMode.Impulse);
 		anim.SetBool ("esta_saltando", true);
 	}
 
-	void OnCollisionEnter(Collision c){
-		if(c.gameObject.tag == "ground"){
+	void OnCollisionEnter(){
 		anim.SetBool ("esta_saltando", false);
-		}
 	}
 }
---
+++ Animation/Assets/Scripts/ButtonScripts/UpButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpButton : MonoBehaviour {
+
+
+	private bool advance;
+	public GameObject obj;
+	// Use this for initialization
+	void Start () {
+		advance = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(advance)
+			obj.GetComponent<PlayerController>().moveForward();
+	}
+
+	void IsPressed(){
+		Debug.Log ("entramos MouseDown");
+		obj.GetComponent<PlayerController>().moveForward();
+	}
+
+	void OnClick(){
+		Debug.Log ("entramos OnClick");
+	}
+
+	void OnPointerDown(){
+		Debug.Log ("entramos MouseDown");
+	}
+}
