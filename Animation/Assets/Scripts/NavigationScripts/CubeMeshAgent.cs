41152e9f5d1e0aae009a36f3fa3f95f38c454b7d
gonzalo <gtercerogarcia@gmail.com>
2018-02-12 18:27:29 +0100

    change color added

--- Animation/Assets/Scripts/NavigationScripts/CubeMeshAgent.cs
+++ Animation/Assets/Scripts/NavigationScripts/CubeMeshAgent.cs
@@ -25,10 +25,11 @@ public class CubeMeshAgent : MonoBehaviour {
 	}
 
 	void Update(){
-		Debug.Log (Vector3.Distance(actualWayPoint.position, this.transform.position));
+		//Debug.Log (Vector3.Distance(actualWayPoint.position, this.transform.position));
 		if( iaAgent.remainingDistance < 2.5f){
 			UpdateActualWayPoint ();
 		}
+		CheckIfPlayerIsNear ();
 	}
 
 	void UpdateActualWayPoint(){
@@ -40,5 +41,24 @@ public class CubeMeshAgent : MonoBehaviour {
 		iaAgent.SetDestination (actualWayPoint.position);
 	}
 
+	private bool CheckIfPlayerIsNear(){
+		bool playerIsNear = false;
+		int layerMask = 1 << 8;
+		Collider[] listOfColliders = Physics.OverlapSphere (this.transform.position, 20.0f, layerMask);
 
+		int index = 0;
+
+		while(index <listOfColliders.Length){
+			//if (listOfColliders [index].name == "Body") {
+				//playerIsNear = true;
+				Renderer _m = listOfColliders [index].gameObject.GetComponent<Renderer> ();
+				if (_m != null) {
+					Debug.Log ("Color Changed");
+					_m.material.color = Color.blue;
+				}
+			//} 
+			index++;
+		}
+		return playerIsNear;
+	}
 }
